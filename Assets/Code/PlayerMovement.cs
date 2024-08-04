2398a41b92956152c0a99e4b6a8edb2d414ab5b4
MJ Sidik <darkimanity@gmail.com>
2024-08-04 17:08:04 +0700

    Improve camera body

--- Assets/Code/PlayerMovement.cs
+++ Assets/Code/PlayerMovement.cs
@@ -2,20 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Cinemachine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 6.0f;
     [SerializeField] Transform cam;  // Kamera
     [SerializeField] Transform body; // Badan Pemain
-    [SerializeField] float mouseSensitivity = 2.0f;
+    [SerializeField] CinemachineVirtualCamera virtualCamera; // Cinemachine virtual camera
+    [Range(0f, 10.0f)] [SerializeField] float speed = 6.0f; // Speed move
+    [Range(0f, 3.0f)] [SerializeField] float mouseSensitivity = 2.0f; // Mouse sens
     private float gravity = -9.81f;
     private CharacterController characterController;
-
     private Vector3 velocity;
     private PlayerInputActions inputActions;
     private Vector2 moveInput;
     private float xRotation = 0f;
+    private CinemachinePOV povComponent;
 
     void Awake()
     {
@@ -27,6 +29,12 @@ public class PlayerMovement : MonoBehaviour
         // Lock the cursor to the center of the screen and hide it
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+    }
+
+    void Start()
+    {
+        // Get the POV component from the Cinemachine virtual camera
+        povComponent = virtualCamera.GetCinemachineComponent<CinemachinePOV>();
     }
 
     void OnEnable()
@@ -85,7 +93,6 @@ public class PlayerMovement : MonoBehaviour
     void HandleCameraRotation()
     {
         // Get mouse input for camera rotation
-        float mouseX = Mouse.current.delta.x.ReadValue() * mouseSensitivity * Time.deltaTime;
         float mouseY = Mouse.current.delta.y.ReadValue() * mouseSensitivity * Time.deltaTime;
 
         // Update vertical camera rotation (pitch)
@@ -93,8 +100,8 @@ public class PlayerMovement : MonoBehaviour
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
-        // Update horizontal body rotation (yaw)
-        float yRotation = cam.eulerAngles.y;
+        // Get the horizontal axis value from the Cinemachine POV component
+        float yRotation = povComponent.m_HorizontalAxis.Value;
         body.rotation = Quaternion.Euler(0f, yRotation, 0f);
 
         // Debug logs to check values
